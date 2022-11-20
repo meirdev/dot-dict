75455fd71067032cf704e0fe062a7b2ad978947e
meirdev <maornet@gmail.com>
2022-11-20 10:29:46 +0200

    fix type

--- dot_dict.py
+++ dot_dict.py
@@ -7,7 +7,7 @@ def default_is_value(obj: Any) -> bool:
 
 class DotDict:
     def __init__(
-        self, obj: dict[Any, Any], is_value_fn: Callable[[Any], bool] = default_is_value
+        self, obj: Any, is_value_fn: Callable[[Any], bool] = default_is_value
     ) -> None:
         self._obj = obj
         self._is_value_fn = is_value_fn
